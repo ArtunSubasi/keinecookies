013ef0c17ea61619163426a9148d4c8a87ef2380
Artun Subasi <asubasi@gmail.com>
2020-04-07 00:05:20 +0200

    Add first page with npm, Vue and Bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # keinecookies
-Source Code of the Website keinecookies.de
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div id="warnung-box" class="row">
+        <div class="col-sm-12">
+            <div class="warnung header">Warnung!</div>
+            <p>
+              Diese Seite verwendet<br/>
+              <span class="warnung">KEINE</span> Cookies!<span class="hide">*</span>
+            </p>
+        </div>
+      </div>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+require('typeface-open-sans')
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Open Sans", Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* background-image: url("./assets/3-cookies.jpg"); */
+  background-repeat: no-repeat;
+  text-align: center;
+}
+#warnung-box {
+  background-color: #222;
+}
+.warnung {
+  color: #db1f1f;
+  font-weight: bold;
+}
+.header {
+  font-size: 3em;
+}
+@media(min-width:576px) {
+  .header {
+    font-size: 5em;
+  }
+}
+p {
+  font-size: 2em;
+  color: #f7ad3e;
+}
+.hide {
+  opacity: 40%;
+}
+</style>
